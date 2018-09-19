a24d25ea678f02de7fc4b639b462f357db680a49
Wesley Pettyjohn <w.a.pettyjohn@gmail.com>
2018-09-19 17:13:18 -0400

    Add comments

--- src/Form.js
+++ src/Form.js
@@ -8,7 +8,7 @@ export default class Form extends React.Component {
 		event.preventDefault();
 		const rows = this.state.data.split("\n");
 		let nodes = [];
-		for (let row of rows) {
+		for (const row of rows) {
 			const cols = row.split(",");
 			const parentId = parseInt(cols[3]);
 			const newNode = new Node({
@@ -17,14 +17,22 @@ export default class Form extends React.Component {
 				shape: cols[2],
 				children: [],
 			});
+			/*
+			 * we need to push the newNode onto the nodes array before setting its
+			 * parent in case it's the root, in which case it will be its own parent
+			 */
 			nodes.push(newNode);
 
 			const newNodeId = newNode.props.id;
+			/*
+			 * now that the newNode instance has been added to the nodes array,
+			 * we can safely look the the newNode's parent
+			 */
 			const parent = nodes.filter(
 				node => node.props.id === parentId
 			)[0]; // get parent node
-			const parentsChildren = parent.props.children;
-			parentsChildren.push(newNodeId); // push id to array of parent's children
+			const siblings = parent.props.children; // you can be your own sibling
+			siblings.push(newNodeId); // push id to array of siblings
 		}
 
 		// root is unique element with self as child node (self-loop)

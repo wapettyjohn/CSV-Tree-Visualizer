a6c5814e8456e25a9f87e94b3529184fd3a7469e
Wesley Pettyjohn <w.a.pettyjohn@gmail.com>
2018-09-21 12:21:17 -0400

    Move component creation from App to Tree and simplify code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Form from './Form.js';
-import Vertex from './Vertex.js';
+import Tree from './Tree.js';
 
 export default class App extends React.Component {
 	state = {
 		data: '',
-		root: '',
+		root: {children:[]}
 	}
 
 	onUpdate = (val) => {
@@ -15,56 +15,57 @@ export default class App extends React.Component {
   };
 
 	handleData = (data) => {
+		console.log('Handling data...');
 		let vertices = [];  // init array of vertices to build
 		const rows = data.split('\n'); // split text into rows
 		for (const row of rows) {
 			const cols = row.split(','); // split each row into columns
-			const newVertex = new Vertex({
+			const newVertex = {
 				id: parseInt(cols[0]),
 				color: cols[1],
 				shape: cols[2],
 				children: [],
-			});
+			};
 			/*
 			 * push newVertex onto vertices array before setting its parent
 			 * in case it's the root, when it will be its own parent
 			 */
 			vertices.push(newVertex);
-
 			/*
 			 * now that newVertex instance is added to vertices array
 			 * we can safely look for the newVertex's parent
 			 */
 			const parent = vertices.filter(
-				vertex => vertex.props.id === parseInt(cols[3]) // find parent by id
+				vertex => vertex.id === parseInt(cols[3]) // find parent by id
 			)[0]; // get parent vertex
-			const siblings = parent.props.children; // you can be your own sibling
-			siblings.push(newVertex); // push newVertex to array of siblings
+			if (newVertex != parent) {
+				parent.children	.push(newVertex); // push newVertex to array of siblings
+			}
 		}
 
 		return vertices;
 	}
 
 	getRoot = (vertices) => {
-		// root is unique element with self as child vertex (self-loop)
+		console.log('Getting root vertex...');
+		// root is the unique element with itself as a child vertex (self-loop)
 		return vertices.filter(
-			vertex => vertex.props.children[vertex.props.id]
+			vertex => vertex.children[vertex.id]
 		)[0];
 	}
 
 	onSubmit = () => {
+		console.log('Data submitted...');
 		const vertices = this.handleData(this.state.data);
 		const root = this.getRoot(vertices);
-		// recursively build tree from root
-
-		console.log(root);
+		this.setState({root: root});
 	}
 
 	render() {
 		return (
 			<div id='App'>
 				<Form onUpdate={this.onUpdate} onSubmit={this.onSubmit}/>
-				<Vertex passedVal='foo' />
+				<Tree root={this.state.root} />
 			</div>
 		);
 	}

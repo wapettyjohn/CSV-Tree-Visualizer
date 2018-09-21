1e19d04f66fa3eafa759552c542c9a579009f437
Wesley Pettyjohn <w.a.pettyjohn@gmail.com>
2018-09-21 12:20:04 -0400

    Move logic from Vertex to Tree and implement recursive render

--- src/Tree.js
+++ src/Tree.js
@@ -1,5 +1,31 @@
 import React from 'react';
-import Node from './Node';
 
 export default class Tree extends React.Component {
+	renderTree(root) {
+		if (root.children) {
+			return (
+				<div>
+					<strong>{root.id}</strong>
+					<ul>
+						{root.children.map(child => (
+							<li key={child.id}>
+								{this.renderTree(child)}
+							</li>
+						))}
+					</ul>
+				</div>
+			);
+		}
+		else {
+			return <span>{root.id}</span>;
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				{this.renderTree(this.props.root)}
+			</div>
+		);
+	}
 }
